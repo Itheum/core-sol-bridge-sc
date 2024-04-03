1861966d930b0a5ee533ecb4827f63c6e652eaa9
Bucur David <dbucur22@gmail.com>
2024-04-03 14:25:42 +0300

    feat: initial commit

---
+++ programs/bridge-program/src/lib.rs
@@ -0,0 +1,32 @@
+use anchor_lang::{prelude::*, solana_program};
+use solana_program::pubkey::Pubkey;
+mod contexts;
+use contexts::*;
+mod states;
+
+declare_id!("HmW1m2rdNRGxRGnCrLavoe7QuWUJb1R2Tgf1nVgji4Sm");
+
+#[program]
+pub mod bridge_program {
+
+    use crate::contexts::AddTokenToWhitelist;
+
+    use super::*;
+
+    pub fn add_to_whitelist(
+        ctx: Context<AddTokenToWhitelist>,
+        mint_of_token_whitelisted: Pubkey,
+    ) -> Result<()> {
+        ctx.accounts
+            .add_to_whitelist(&ctx.bumps, mint_of_token_whitelisted)
+    }
+
+    pub fn initialize_bridge(ctx: Context<InitializeBridge>, amount: u64) -> Result<()> {
+        ctx.accounts.initialize_bridge(&ctx.bumps, amount)?;
+        ctx.accounts.deposit(amount)
+    }
+
+    pub fn bridge_and_close(ctx: Context<FinalizeBridge>) -> Result<()> {
+        ctx.accounts.burn_and_close_vault()
+    }
+}
